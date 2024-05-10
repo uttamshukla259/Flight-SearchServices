2a53990119cf8b8d8cc08fe65d9e6e0e889a8c97
uttamshukla259 <uttamshukla259@gmail.com>
2024-05-10 08:36:52 +0530

    db setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser")
 
 const { PORT } = require('./config/serverConfig');
 
@@ -11,9 +12,12 @@ const setupAndStartServer = async () => {
     app.get('/', (req, res)=>{
         res.send("hello")
     })
-  app.listen(PORT, () => {
-    console.log(`Server starting at ${PORT}`);
+   
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }))
   
+  app.listen(PORT, () => {  
+    console.log(`Server starting at ${PORT}`);
   });
 };
 
